54a1cd6cfba73b39baaf3f8e5831b78a9f6859fe
unknown <david.silva425@etec.sp.gov.br>
2022-06-16 03:50:09 -0300

    Portifolio desenvolvido por David Soares Silva

---
+++ JS.js
@@ -0,0 +1,15 @@
+//const target = document.querySelectorAll('[data-anime]');
+//const animationClass = 'animate';
+
+function animeWelcome(){
+    
+}
+window.addEventListener('resize', function(){
+    animeWelcome();
+})
+
+function mudarCor(){
+    document.body.style.background = red;
+
+
+}
